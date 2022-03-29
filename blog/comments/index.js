fcf0d66a641252378860bf3daeb118f52a4f9952
Trevor Lawrence <tlawren3@icloud.com>
2022-03-29 09:19:14 -0700

    comment moderation implemented

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -20,24 +20,52 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content: content });
+  comments.push({ id: commentId, content: content, status: "pending" });
 
-  await axios.post("http://localhost:4005/events", {
-    type: "CommentCreated",
-    data: {
-      id: commentId,
-      content,
-      postId: req.params.id,
-    },
-  });
+  await axios
+    .post("http://localhost:4005/events", {
+      type: "CommentCreated",
+      data: {
+        id: commentId,
+        content,
+        postId: req.params.id,
+        status: "pending",
+      },
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 
   commentsByPostId[req.params.id] = comments;
 
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("event received", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content: comment.content,
+      },
+    });
+  }
 
   res.send({});
 });
