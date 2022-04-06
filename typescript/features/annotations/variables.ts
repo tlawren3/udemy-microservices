f9de0739998abbc9b691151e66a9d1d226b38f04
Trevor Lawrence <tlawren3@icloud.com>
2022-04-06 08:21:05 -0600

    home mac ts updates, switching to other stuff.

---
+++ typescript/features/annotations/variables.ts
@@ -0,0 +1,12 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+
+// array of objects
+let colors: string[] = ["red", "green", "blue"];
