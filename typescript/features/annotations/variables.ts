375d7ca73e18646d02383ec804ec4c353506c0a7
Trevor Lawrence <tlawren3@icloud.com>
2022-04-10 20:53:59 -0600

    end of vacation - pushing changes to repo

--- typescript/features/annotations/variables.ts
+++ typescript/features/annotations/variables.ts
@@ -10,3 +10,28 @@ let now: Date = new Date();
 
 // array of objects
 let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, false];
+
+// classes
+class Car {
+
+}
+let car: Car = new Car();
+
+// object literals
+let point: { x: number; y: number } = {
+    x: 10,
+    y: 20
+};
+
+// function
+const logNumber: (i: number) => void = (i: number) => {
+    console.log(i);
+};
+
+// when to use annotations
+// 1) Function that returns type 'any'
+const json = '{"x": 10, "y": 20}';
+const coords = JSON.parse(json);
+console.log(coords); // {x: 10, y: 20};
